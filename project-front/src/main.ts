0e9f891fe364af6ac6d06df9086982312a40ebcf
Nelsiemon <florent.roux@meritis.fr>
2023-01-09 22:42:35 +0100

    Add Element plus to front end

--- project-front/src/main.ts
+++ project-front/src/main.ts
@@ -1,14 +1,22 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import 'element-plus/dist/index.css';
+import { createPinia } from 'pinia';
 
-import App from "./App.vue";
-import router from "./router";
+import App from './App.vue';
+import router from './router';
 
-import "./assets/main.css";
+import './assets/main.css';
 
 const app = createApp(App);
 
+app.use(ElementPlus);
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
+app.mount('#app');
